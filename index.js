eddb81275872a042acda2d16abd7185c7714a5cb
Teodora Gjini <teodoragjini@yahoo.com>
2022-06-15 02:26:02 +0200

    changes

--- index.js
+++ index.js
@@ -4,22 +4,31 @@ let distance = Number(prompt("How far are you traveling?"))
 let priceForTravelKm = 0.21
 let initialPrice = priceForTravelKm*distance
 
-
-
 let taxUnder18 = 0.2 
 let taxOver65 = 0.4
 
+let discountPrice
+
 if (age <= 18){
-  discountPrice =  initialPrice - (taxUnder18*initialPrice)
+    let discount = taxUnder18 * 100
+    let price = (initialPrice - (taxUnder18*initialPrice)).toFixed(2)
+    
+    discountPrice = `You have a ${discount}% discount and you will now pay $${price}`
 } 
 
 else if (age >= 65){
-   discountPrice = initialPrice - (taxOver65*initialPrice)
+    let discount = taxOver65 * 100
+    let price = (initialPrice-(taxOver65*initialPrice)).toFixed(2)
+
+   discountPrice =  `You have a ${discount}% discount and you will now pay $${price}`
 }
 
 else {
-    discountPrice = initialPrice
+    discountPrice = `You have no discount and you will pay  $${initialPrice}`
 }
 
 
 console.log(discountPrice)
+
+
+
